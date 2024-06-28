9813a7061358064817853e765aa8ab4f6690e91c
FerushT <ferush82@gmail.com>
2024-06-28 15:21:42 +0200

    solved 28.06.2024

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
@@ -4,6 +4,38 @@ class S4535 extends StatelessWidget {
   const S4535({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.red,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue,
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.green,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.yellow,
+            )
+          ],
+        )
+      ],
+    );
   }
 }
